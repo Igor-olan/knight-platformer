bd38edcee190f990308bcf631485fb27c6ca05c3
Igor-olan <olangan4238q@gmail.com>
2025-10-23 15:55:14 +0700

    feat: brightness overlay & audio fix

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -5,6 +5,9 @@ extends Panel
 @onready var bright: HSlider = $bright
 @onready var back: Button = $back
 @onready var settings: Panel = $"."
+@onready var sfx: HSlider = $sfx
+@onready var brightness_overlay: ColorRect = $brightnessOverlay
+@onready var pickup: AudioStreamPlayer2D = $Pickup
 
 func _ready() -> void:
 	settings.visible = false
@@ -32,5 +35,11 @@ func _on_main_changed() -> void:
 func _on_bgm_changed() -> void:
 	pass # Replace with function body.
 
-func _on_bright_changed() -> void:
-	pass # Replace with function body.
+func _on_sfx_value_changed(sfx_value: float) -> void:
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), linear_to_db(sfx_value))
+
+func _on_sfx_drag_ended(value_changed: bool) -> void:
+	pickup.play()
+
+func _on_bright_value_changed(value: float) -> void:
+	brightness_overlay.set_brightness(value)
